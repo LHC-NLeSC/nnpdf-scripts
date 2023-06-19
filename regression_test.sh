bc7bc756eae8befd4589af6854ad20b030df50c6
Aron Jansen <aronpjansen@gmail.com>
2023-06-19 09:59:17 +0200

    Add evolven3fit to regression_test.sh

--- regression_test.sh
+++ regression_test.sh
@@ -18,7 +18,15 @@ REPLICAS=$4
 RUNCARD1="${RUNCARD}_%{BRANCH1}" 
 RUNCARD2="${RUNCARD}_%{BRANCH2}" 
 
+echo "Evolving all replicas from runcard ${RUNCARD1}..."
+evolven3fit $RUNCARD1 $REPLICAS
+echo "Evolving all replicas from runcard ${RUNCARD2}..."
+evolven3fit $RUNCARD2 $REPLICAS
+
+echo "Running postfit for runcard ${RUNCARD1}..."
 postfit $RUNCARD1 $REPLICAS
+echo "Running postfit for runcard ${RUNCARD2}..."
 postfit $RUNCARD2 $REPLICAS
 
+echo "Comparing fits and uploading..."
 vp-comparefits $RUNCARD1 $RUNCARD2 --upload
