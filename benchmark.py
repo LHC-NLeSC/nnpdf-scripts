08e4577f591c04a1a69c215dffddd1c44f6d0c13
Aron <aronpjansen@gmail.com>
2023-08-25 16:25:52 +0200

    Add refactored version

--- benchmark.py
+++ benchmark.py
@@ -66,10 +66,17 @@ def compute(pdf, basis_mask, fk, invcov, mask2):
 
 def compute_replicafirst(pdf, basis_mask, fk, invcov, mask2):
     pdf = tf.squeeze(pdf, axis=0)
-    luminosity = tf.einsum('rai,rbj->rjiba', pdf, pdf)
+    luminosity = tf.einsum('rxf,ryg->rgfyx', pdf, pdf)
     pdf_x_pdf = tf.boolean_mask(luminosity, basis_mask, axis=1)
     ret = tf.einsum('nfij, rfij -> rn', fk, pdf_x_pdf)
     DY_out = op.batchit(ret)  # (BATCH, REPLICAS, NDATA)
+    DY_out_masked = op.tensor_product(DY_out, mask2, axes=1)  # (BATCH, REPLICAS, NDATA_TRAIN)
+    y = DY_out_masked
+    losses = op.einsum("bri, rij, brj -> r", y, invcov, y)  # (REPLICAS,)
+    return losses
+
+def compute_alltogether(pdf, basis_mask, fk, invcov, mask2):
+    DY_out = tf.einsum('brxf, fgF, nFxy, bryg -> brn', pdf, basis_mask, fk, pdf)
     DY_out_masked = op.tensor_product(DY_out, mask2, axes=1)  # (BATCH, REPLICAS, NDATA_TRAIN)
     y = DY_out_masked
     losses = op.einsum("bri, rij, brj -> r", y, invcov, y)  # (REPLICAS,)
@@ -81,6 +88,34 @@ def define_inputs_replicafirst(seed):
     inputs['pdf'] = tf.transpose(inputs['pdf'], perm=[0, 3, 1, 2])
     return inputs
 
+def define_inputs_alltogether(seed):
+    inputs = define_inputs_replicafirst(seed)
+    inputs['basis_mask'] = tensor_from_mask(inputs['basis_mask'])
+    return inputs
+
+def tensor_from_mask(mask):
+    """
+    Create a rank 3 tensor that replicates the functionality of tf.boolean_mask
+
+    Args:
+        mask: a rank 2 boolean tensor
+
+    Returns:
+        rank 3 tensor with shape (mask.shape[0], mask.shape[1], tf.reduce_sum(mask))
+        of zeros and ones such that
+        tf.boolean_mask(tensor, mask, axis=1) == tf.einsum('fg..., fgF -> F...', tensor, tensor_from_mask(mask))
+    """
+    mask_array = []
+    for i in range(mask.shape[0]):
+        for j in range(mask.shape[1]):
+            if mask[i, j] == True:
+                temp_matrix = np.zeros((mask.shape[0], mask.shape[1]))
+                temp_matrix[i, j] = 1
+                mask_array.append(temp_matrix)
+    mask = np.stack(mask_array, axis=-1)
+    mask_tensor = tf.convert_to_tensor(mask)
+    return mask_tensor
+
 def timeit(nreps, create_inputs, function):
     inputs = create_inputs(42)
     start = time.time()
@@ -89,6 +124,18 @@ def timeit(nreps, create_inputs, function):
     end = time.time()
     print(f"{function.__name__} took {end-start} seconds, {(end-start)/nreps} per call")
 
-NREPS = 1_000
+def test_equal():
+    original = compute(**define_inputs(42))
+    replicafirst = compute_replicafirst(**define_inputs_replicafirst(42))
+    alltogether = compute_alltogether(**define_inputs_alltogether(42))
+    # this gives really 0
+    print(f"difference between original and replicafirst: {tf.reduce_sum(original - replicafirst)} (relative: {tf.reduce_sum(original - replicafirst)/tf.reduce_sum(original)})")
+    # this gives 10^-15 for relative
+    print(f"difference between original and alltogether: {tf.reduce_sum(original - alltogether)} (relative: {tf.reduce_sum(original - alltogether)/tf.reduce_sum(original)})")
+
+#test_equal()
+
+NREPS = 10
 timeit(NREPS, define_inputs, compute)
 timeit(NREPS, define_inputs_replicafirst, compute_replicafirst)
+timeit(NREPS, define_inputs_alltogether, compute_alltogether)
